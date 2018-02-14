472d5b80ba1bad4d0417879456b6bf62224048b9
md khan <arifk36@yahoo.com>
2018-02-14 10:13:24 -0500

    made the connection with sql databse

--- index.js
+++ index.js
@@ -5,13 +5,25 @@
        request = require('request'),
        express = require('express'),
        app = express();
+ const ThirdPartyApi = 'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1';
+ const MinimumNumberOfReviews = 5;
+ const MinimumNumberOfRatings = 4.0;
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+ const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+
+// Connecting to the database SQL
+let db = new sqlite.Database(DB_PATH, sqlite.OPEN_READONLY, error => {
+  if (error) {
+    console.error(error.message);
+  }
+  console.log('You are now connected with SQL database.');
+});
+
 
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
