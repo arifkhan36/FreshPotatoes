05208d55fc603e0d64411f9ea144112af3f04ca4
md khan <arifk36@yahoo.com>
2018-02-16 20:49:04 -0500

    paginate by offset and limit the number of returned records both are printing on the screen in JSON format as required

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- // form here
+ // from here
  // install sqlite3 to local machine and setting database
  const sqlite = require('sqlite3'),
        Sequelize = require('sequelize'),
@@ -8,14 +8,11 @@
  const ThirdPartyApi = 'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1';
  const MinimumNumberOfReviews = 5;
  const MinimalRating = 4.0;
-
  const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
-
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
-
 // Connecting the database with SQL
 let db = new sqlite.Database(DB_PATH, sqlite.OPEN_READONLY, error => {
   if (error) {
@@ -23,18 +20,25 @@ let db = new sqlite.Database(DB_PATH, sqlite.OPEN_READONLY, error => {
   }
   console.log('You are now connected with SQL database.');
 });
-
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
-
 // ROUTE HANDLER
-function getFilmRecommendations(req, res) {
-   res.status(500).send('Not Implemented');
+function getFilmRecommendations(req, res, next) {
+   //res.status(500).send('Not Implemented');
    let filmId = req.params.id;
-   limit = 10; //default
-   offset = 0;
+   //ERORR HANLING FOR filmID
+   //................................
+   let limit = req.query.limit
+   let offset = req.query.offset;
+   if (limit === null || isNaN(limit)){limit = 10;} //to default, limit cannit be a null nor string
+   if (offset === null ||isNaN(offset)){let offset = 0;} //default, offset cannot be a null nor string
+   limit =Number(req.query.limit);
+   offset =Number(req.query.offset);
+   if(!Number.isInteger(limit)){limit=10;} //to default, limit cannot be racioanal number
+   if(!Number.isInteger(offset)){offset=0;} //to default, offset cannot be racional number
+   if(limit<0){limit=10;} //to default, limit cannot be negative
+   if(offset<0){offset=10;} //to default, offset cannot be negative
    let data = [];
-
     db.all(`SELECT films.id, films.title, films.release_date,  genres.name FROM films
              INNER JOIN genres ON films.genre_id=genres.id
              WHERE
@@ -62,94 +66,85 @@ function getFilmRecommendations(req, res) {
       //console.log(row.release_date);
       //console.log(row.name);
       //sortFilms();
-
-
     });
     //console.log(data);
     sortFilms(data);
   });
-
-
     function sortFilms(dataDb) {
       let recommendations = [];
       let numberThirdPartyAPIcalls = 0;
-
-
       for(let index=0; index<dataDb.length; index++){
-
-
-      //console.log(dataDb[index]);
-      request('http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films='+dataDb[index].id, function(error, response) {
-        numberThirdPartyAPIcalls++;
-        if(error){
-        console.log(error); //handlin error
-
-
-        //let obj = JSON.parse(response.body);
-        }
-        else{
-          if (JSON.parse(response.body)[0].reviews.length >= MinimumNumberOfReviews){ //condition 1
-          //console.log("object: ", JSON.parse(response.body)[0]); //printing response body
-          //console.log(""); //making distance
-          let object = JSON.parse(response.body)[0];
-          //console.log(object); //printing all reviews
-
-          //calculate average rating of film
-          let averageRating;
-          let reviews = object.reviews;
-          let sum = 0; //sum of ratings needed for calculation
-          for (let i=0; i<reviews.length; i++){
+        //console.log(dataDb[index]);
+        request('http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films='+dataDb[index].id, function(error, response) {
+           numberThirdPartyAPIcalls++;
+           if(error){
+           console.log(error); //handlin error
+          //let obj = JSON.parse(response.body);
+          }
+          else{
+        if (JSON.parse(response.body)[0].reviews.length >= MinimumNumberOfReviews){ //condition 1
+             //console.log("object: ", JSON.parse(response.body)[0]); //printing response body
+             //console.log(""); //making distance
+             let object = JSON.parse(response.body)[0];
+             //console.log(object); //printing all reviews
+            //calculate average rating of film
+            let averageRating;
+            let reviews = object.reviews;
+            let sum = 0; //sum of ratings needed for calculation
+            for (let i=0; i<reviews.length; i++){
             //console.log(reviews[i]);
              sum = sum+reviews[i].rating;
-          }//for
-          averageRating=sum/reviews.length;
-          if (averageRating>MinimalRating){
-          //console.log(averageRating);// printing only ratings that are greater than 4.0
-          //film.averageRating = averageRating;
-          //film.reviews = reviews.length;
-          let filmToRecommend = {};
-          //console.log("data: ", data);
-          filmToRecommend.id = data[index].id;
-          filmToRecommend.title = data[index].title;
-          filmToRecommend.releaseDate = data[index].releaseDate;
-          filmToRecommend.genre = data[index].genre;
-          filmToRecommend.averageRating = Number(averageRating.toFixed(2));
-          filmToRecommend.reviews = reviews.length;
-          recommendations.push(filmToRecommend);
-
-
-
+            }//for
+            averageRating=sum/reviews.length;
+            if (averageRating>MinimalRating){
+            //console.log(averageRating);// printing only ratings that are greater than 4.0
+            //film.averageRating = averageRating;
+            //film.reviews = reviews.length;
+            let filmToRecommend = {};
+            //console.log("data: ", data);
+            filmToRecommend.id = data[index].id;
+            filmToRecommend.title = data[index].title;
+            filmToRecommend.releaseDate = data[index].releaseDate;
+            filmToRecommend.genre = data[index].genre;
+            filmToRecommend.averageRating = Number(averageRating.toFixed(2));
+            filmToRecommend.reviews = reviews.length;
+            recommendations.push(filmToRecommend);
           //filmToRecommend.title = .title;
           //console.log(filmToRecommend);
-
-
-
-
-
           //console.log(numberThirdPartyAPIcalls);
-        }
-        //console.log(numberThirdPartyAPIcalls);
-        //her we have to create response using if statement
+          }
+          //console.log(numberThirdPartyAPIcalls);
+          //her we have to create response using if statement
                   //if(true){
-          if (numberThirdPartyAPIcalls === dataDb.length){
+          if ( dataDb.length === numberThirdPartyAPIcalls){
+            JSONformat(recommendations);
             //console.log(numberThirdPartyAPIcalls);
             //console.log(dataDb.length);
-
-            let obj = {};
-            obj.recommendations = recommendations;
-            obj.meta = {limit: limit, offset: offset};
-            console.log(obj);
+            ////let obj = {};
+            /////obj.recommendations = recommendations;
+            ////obj.meta = {limit: limit, offset: offset};
+            ////console.log(obj);
             //here we have to send our object as response
-          }
-      };
-
+            ////res.json(obj);
+            }
+             // else {
+             //   console.log(error)
+             // }
+          };
+        };
+      })//end of request;
+      function JSONformat(APIData) {
+      let obj = {
+      recommendations: APIData.splice(offset, limit),
+      meta: {limit:limit, offset:offset }
     };
-
-   })//end of request;
-
-
-     }
-       }
+    res.json(obj);
+    }
+    }
+  }
 }
-
+app.use(function(req, res, next) {
+  res.status(404).json({ message: "key missing" });
+  console.log(res.statusCode);
+});
 module.exports = app;
